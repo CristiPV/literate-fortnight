df5087f6f58abb77cd311a5a6c1aafbffa1fdba9
Cristian <cristi.valentin29@gmail.com>
2021-08-03 10:56:34 +0200

    Add environment variables to frontend app

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:4001";
+
+const ENDPOINT = process.env.REACT_APP_SERVER_ENDPOINT;
+
 function App() {
   const [response, setResponse] = useState("");
 
