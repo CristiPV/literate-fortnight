0362e2545479abd773c4b6573ffa74d4d0c9cd51
Jonatan Bakke <cph-jb281@cphbusiness.dk>
2021-08-03 10:25:16 +0200

    initial work for socket in frontend

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,7 +1,21 @@
-
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4001";
 function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", (data) => {
+      setResponse(data);
+    });
+  }, []);
+
   return (
     <div>
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
     </div>
   );
 }
