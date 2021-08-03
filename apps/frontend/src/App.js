04ec79e6e2aaaf3e17f771bc98c5f749e9ede392
Cristian <cristi.valentin29@gmail.com>
2021-08-03 11:10:29 +0200

    Extract ClientSocket into its own component

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,24 +1,14 @@
-import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
+import React from "react";
+
+import ClientSocket from "./components/ClientSocket";
 
 const ENDPOINT = process.env.REACT_APP_SERVER_ENDPOINT;
 
 function App() {
-  const [connectionTime, setConnectionTime] = useState("");
-
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT, {transports: ['websocket']});
-    socket.on("sendTime", (data) => {
-      setConnectionTime(data);
-    });
-  }, []);
-
   return (
-    <div>
-      <p>
-        It's <time dateTime={connectionTime}>{connectionTime}</time>
-      </p>
-    </div>
+    <>
+      <ClientSocket />
+    </>
   );
 }
 
---
+++ apps/frontend/src/components/ClientSocket.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+
+const ENDPOINT = process.env.REACT_APP_SERVER_ENDPOINT;
+
+function ClientSocket() {
+  const [connectionTime, setConnectionTime] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT, { transports: ["websocket"] });
+    socket.on("sendTime", (data) => {
+      setConnectionTime(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <p>
+        It's <time dateTime={connectionTime}>{connectionTime}</time>
+      </p>
+    </div>
+  );
+}
+
+export default ClientSocket;
