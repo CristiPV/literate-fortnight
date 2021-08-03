c307d36f2f5187806b651599df3f74e71ce16fa5
Cristian <cristi.valentin29@gmail.com>
2021-08-03 10:41:17 +0200

    Set up socket.io
    Implement socket.io and send the time of connection to client sockets.

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -1,10 +1,32 @@
 require('dotenv').config();
+const socketIo = require("socket.io");
 const express = require("express");
-const app = express();
 
+// Environment variables
 const PORT = process.env.PORT || 8080;
 
-const server = app.listen(PORT, (error) => {
+// Routes
+const index = require("./routes/index").router;
+
+const app = express();
+
+app.use(index);
+
+const httpServer = http.createServer(app);
+const io = socketIo(httpServer);
+
+io.on("connection", (socket) => {
+    console.log("Socket connected", socket.id);
+
+    // Send the time of connection to the client
+    socket.emit("sendTime", new Date());
+
+    socket.on("disconnect", () => {
+        console.log("Socket disconnected", socket.id);
+    });
+});
+
+const server = httpServer.listen(PORT, (error) => {
   if (error) console.log(error);
 
   console.log("The server has started on port", server.address().port);
